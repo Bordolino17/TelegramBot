cdaff5fca101e38b213cb60d1ecadb41a614db31
Bordolino17 <33183383+Bordolino17@users.noreply.github.com>
2020-08-08 17:04:47 -0300

    Audio youtube

---
+++ src/youtube.py
@@ -0,0 +1,13 @@
+import youtube_dl
+
+def get_mp3(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
